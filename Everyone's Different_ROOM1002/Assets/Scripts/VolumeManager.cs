9c1cd99512e6bcb3ffe9a41d73c0e09073ef748a
onlyBibiesca <vikktourr@gmail.com>
2025-03-15 23:16:24 +0800

    Audio
    Added BG Music
    Added Music Volume

---
+++ Everyone's Different_ROOM1002/Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVoume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    public void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
