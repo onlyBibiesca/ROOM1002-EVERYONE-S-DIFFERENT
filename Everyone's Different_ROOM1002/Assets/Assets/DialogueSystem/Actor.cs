988be0e3adc01f628bed1febdbcaa35a04cf213a
onlyBibiesca <vikktourr@gmail.com>
2025-03-19 19:18:48 +0800

    Working Dialogue System (Prototype)

---
+++ Everyone's Different_ROOM1002/Assets/Assets/DialogueSystem/Actor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    public string Name;
+    public Dialogue dialogue;
+
+    private void OnClick()
+    {
+        Debug.Log("Speaking to " + Name);
+        SpeakTo();
+    }
+
+    public void SpeakTo()
+    {
+        DialogueManager.Instance.StartDialogue(Name, dialogue.RootNode);
+    }
+}
