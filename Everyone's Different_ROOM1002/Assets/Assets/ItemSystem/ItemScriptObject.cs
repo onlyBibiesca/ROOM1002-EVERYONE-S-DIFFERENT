0eb33437ea706a68ba777e127890b2ba69bb358d
VooKak <sethlinco@gmail.com>
2025-04-02 21:37:47 +0800

    Stat System P1

--- Everyone's Different_ROOM1002/Assets/Assets/ItemSystem/ItemScriptObject.cs
+++ Everyone's Different_ROOM1002/Assets/Assets/ItemSystem/ItemScriptObject.cs
@@ -3,58 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 
 [CreateAssetMenu]
 public class ItemScriptObject : ScriptableObject
 {
     [Header("Item")]
     public string itemName;
-    public List<StatToChange> statchange;
-    public StatToChange statToChange = new StatToChange();
-    public int amountChangeStat;
+    //public List<StatToChange> statchange;
+    public List<StatChange> statChanges = new List<StatChange>();
+    //public StatToChange statToChange = new StatToChange();
+    //public int amountChangeStat;
+    
 
     [Header("PlayerStats")]
     public PlayerStats playerStats;
 
+    public List<ItemStats> itemstat;
 
     public void UseItem()
     {
 
         PlayerManager playerManager = FindObjectOfType<PlayerManager>();
 
-        if (statToChange == StatToChange.health)
+        
+        foreach (StatChange change in statChanges)
         {
-            //example codes if stats are scripted
-            //GameObject.Find("HealthManager").GetComponent<Health>().ChangeHealth(amountChangeStat);
-            //healthSlider.value += playerStats.hunger;
-            playerManager.ChangeHealth(amountChangeStat);
-            Debug.Log("Health Added");
-        }
 
-        if (statToChange == StatToChange.hunger)
-        {
-            playerManager.ChangeHunger(amountChangeStat);
-            Debug.Log("Hunger Added");
-        }
+            switch (change.stat)
+            {
+                case StatToChange.health:
+                    playerManager.ChangeHealth(change.amount);
+                    Debug.Log("Health Added: " + change.amount);
+                    break;
 
-        if (statToChange == StatToChange.energy)
-        {
-            //GameObject.Find("StaminaManager").GetComponent<Stamina>().ChangeStamina(amountChangeStat);
-            playerManager.ChangeEnergy(amountChangeStat);
-            Debug.Log("Stamina Added");
-        }
+                case StatToChange.hunger:
+                    playerManager.ChangeHunger(change.amount);
+                    Debug.Log("Health Added: " + change.amount);
+                    break;
 
-        if (statToChange == StatToChange.socialbatt)
-        {
-            //GameObject.Find("StaminaManager").GetComponent<Stamina>().ChangeStamina(amountChangeStat);
-            playerManager.ChangeSocialBattery(amountChangeStat);
-            Debug.Log("Social Battery Added");
+                case StatToChange.energy:
+                    playerManager.ChangeEnergy(change.amount);
+                    Debug.Log("Health Added: " + change.amount);
+                    break;
+
+                case StatToChange.socialbatt:
+                    playerManager.ChangeSocialBattery(change.amount);
+                    Debug.Log("Health Added: " + change.amount);
+                    break;
+
+            }
         }
+            /*
+           if (statToChange == StatToChange.health)
+           {
+               //example codes if stats are scripted
+               //GameObject.Find("HealthManager").GetComponent<Health>().ChangeHealth(amountChangeStat);
+               //healthSlider.value += playerStats.hunger;
+               playerManager.ChangeHealth(amountChangeStat);
+               Debug.Log("Health Added");
+           }
+
+           if (statToChange == StatToChange.hunger)
+           {
+               playerManager.ChangeHunger(amountChangeStat);
+               Debug.Log("Hunger Added");
+           }
+
+           if (statToChange == StatToChange.energy)
+           {
+               //GameObject.Find("StaminaManager").GetComponent<Stamina>().ChangeStamina(amountChangeStat);
+               playerManager.ChangeEnergy(amountChangeStat);
+               Debug.Log("Stamina Added");
+           }
+
+           if (statToChange == StatToChange.socialbatt)
+           {
+               //GameObject.Find("StaminaManager").GetComponent<Stamina>().ChangeStamina(amountChangeStat);
+               playerManager.ChangeSocialBattery(amountChangeStat);
+               Debug.Log("Social Battery Added");
+           }
+        
+            */
+        
     }
 
 
-
-
+    [System.Serializable]
+    public class StatChange
+    {
+        public StatToChange stat;
+        public int amount;
+    }
     //sets the item stats
     public enum StatToChange
     {
---
+++ Everyone's Different_ROOM1002/Assets/Assets/ItemSystem/ItemStats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class ItemStats
+{
+    /*
+    public ItemScriptObject itemscriptobj;
+    public List<itemscriptobj.StatToChange> statchange;
+    public struct amountStat
+    {
+
+        public int amount;
+
+    }
+    
+    */
+}
